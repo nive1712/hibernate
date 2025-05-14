d7eecc738d4ab6d5592f7594cd4b4955805a78f4
S.NIVEDHA <89909984+nive1712@users.noreply.github.com>
2025-05-14 14:43:44 +0530

    Create Ticket.java

---
+++ hibernate-task-jpa/src/main/java/com/example/model/Ticket.java
@@ -0,0 +1,67 @@
+package com.example.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "movie_name")
+    private String movieName;
+
+    @Column(name = "seat_number")
+    private String seatNumber;
+
+    @Column(name = "buyer_name")
+    private String buyerName;
+
+    public Ticket() {}
+
+    public Ticket(String movieName, String seatNumber, String buyerName) {
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.buyerName = buyerName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public String getBuyerName() {
+        return buyerName;
+    }
+
+    public void setBuyerName(String buyerName) {
+        this.buyerName = buyerName;
+    }
+}
